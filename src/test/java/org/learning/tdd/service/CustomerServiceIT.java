a87eb53292de8e74fa9306fb08439ee63815c710
GG <GG1D29@users.noreply.github.com>
2023-09-21 11:44:03 +0700

    add more test case for Integration testing

--- src/test/java/org/learning/tdd/service/CustomerServiceIT.java
+++ src/test/java/org/learning/tdd/service/CustomerServiceIT.java
@@ -1,6 +1,7 @@
 package org.learning.tdd.service;
 
 import org.junit.jupiter.api.Test;
+import org.learning.tdd.exception.NotFoundException;
 import org.learning.tdd.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
 @SpringBootTest
@@ -21,5 +23,18 @@ public class CustomerServiceIT {
     void getAllCustomers() {
         List<Customer> customers = customerService.getAllCustomers();
         assertThat(customers.size()).isEqualTo(5);
+    }
+
+    @Test
+    void getCustomer() {
+        Customer customer = customerService.getCustomer("penatibus.et@lectusa.com");
+        assertThat(customer.getFirstName()).isEqualTo("Cally");
+        assertThat(customer.getLastName()).isEqualTo("Reynolds");
+    }
+
+    @Test
+    void getCustomer_NotFound() {
+        Exception e = assertThrows(NotFoundException.class, () -> customerService.getCustomer("someone@lectusa.com"));
+        assertThat(e.getMessage()).isEqualTo("no user found with email someone@lectusa.com");
     }
 }
